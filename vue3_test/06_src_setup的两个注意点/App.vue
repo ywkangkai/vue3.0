d6253a19cba8ae416993cacf0ef5929632033242
kai.kang <kangkai@1314>
2022-02-27 10:24:12 +0800

    computed，watch，生命周期，hook

---
+++ vue3_test/06_src_setup的两个注意点/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Demo @hello="showMsg" msg="你好" school="学习">
+    <template v-slot:qwe>
+      <span >娃哈哈哈哈哈哈</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from "@/components/Demo";
+export default {
+  name: 'App',
+  components: {Demo},
+  setup(){
+    function showMsg(value1,value2){
+      alert(`接收到参数${value1},${value2}`)
+    }
+    return{
+      showMsg
+    }
+  }
+}
+</script>
+
---
+++ vue3_test/10_src_生命周期/components/Demo.vue
@@ -0,0 +1,68 @@
+<template>
+	<h2>当前求和为：{{sum}}</h2>
+	<button @click="sum++">点我+1</button>
+</template>
+
+<script>
+	import {ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from 'vue'
+	export default {
+		name: 'Demo',
+		
+		setup(){
+			console.log('---setup---')
+			//数据
+			let sum = ref(0)
+
+			//通过组合式API的形式去使用生命周期钩子
+			onBeforeMount(()=>{
+				console.log('---挂载前---')
+			})
+			onMounted(()=>{
+				console.log('---挂载前后---')
+			})
+			onBeforeUpdate(()=>{
+				console.log('---更新前---')
+			})
+			onUpdated(()=>{
+				console.log('---更新后---')
+			})
+			onBeforeUnmount(()=>{
+				console.log('---删除前---')
+			})
+			onUnmounted(()=>{
+				console.log('---删除后---')
+			})
+
+			//返回一个对象（常用）
+			return {sum}
+		},
+		//通过配置项的形式使用生命周期钩子
+		//#region 
+		beforeCreate() {
+			console.log('---beforeCreate---')
+		},
+		created() {
+			console.log('---created---')
+		},
+		beforeMount() {
+			console.log('---beforeMount---')
+		},
+		mounted() {
+			console.log('---mounted---')
+		},
+		beforeUpdate(){
+			console.log('---beforeUpdate---')
+		},
+		updated() {
+			console.log('---updated---')
+		},
+		beforeUnmount() {
+			console.log('---beforeUnmount---')
+		},
+		unmounted() {
+			console.log('---unmounted---')
+		},
+		//#endregion
+	}
+</script>
+
